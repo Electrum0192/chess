81506b2f98d059ce3dd01acacbfec4f770f2fc8d
Dean Bristow <dean.bristow@gmail.com>
2024-03-18 17:11:35 -0600

    Implement Help Command

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -40,7 +40,7 @@ public class Main {
             //Read command and create the http request
             if(setting.equals("LOGGED_OUT")) {
                 if (readEqual(action, "Help")) {
-
+                    System.out.println(helpText(setting));
                 } else if (readEqual(action, "Quit")) {
                     run = false;
                 } else if (readEqual(action, "Login")) {
@@ -51,18 +51,22 @@ public class Main {
                     throw new Exception("Unknown Command");
                 }
             } else if (setting.equals("LOGGED_IN")) {
-                if(readEqual(action,"Help")){
+                if (readEqual(action, "Help")) {
+                    System.out.println(helpText(setting));
+                } else if (readEqual(action, "Logout")) {
 
-                } else if (readEqual(action,"Logout")) {
+                } else if (readEqual(action, "Create")) {
 
-                } else if (readEqual(action,"Create") && readEqual(command[1],"Game")) {
+                } else if (readEqual(action, "List")) {
 
-                } else if (readEqual(action,"List") && readEqual(command[1],"Games")) {
+                } else if (readEqual(action, "Join")) {
 
-                } else if (readEqual(action,"Join") && readEqual(command[1],"Game")) {
+                } else if (readEqual(action, "Observe")) {
 
-                } else if (readEqual(action,"Join") && readEqual(command[1],"Observer")) {
-
+                } else if (readEqual(action, "Quit")) {
+                    //Logout
+                    //Quit Game
+                    run = false;
                 }else{
                     throw new Exception("Unknown Command");
                 }
@@ -77,5 +81,71 @@ public class Main {
         String low = input.toLowerCase();
         String testLow = test.toLowerCase();
         return low.equals(testLow);
+    }
+
+    private static String helpText(String setting){
+        if(setting.equals("LOGGED_OUT")){
+            StringBuilder builder = new StringBuilder();
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  register <USERNAME> <PASSWORD> <EMAIL>");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to create an account\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  login <USERNAME> <PASSWORD>");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to login and start playing\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  quit");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to exit\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  help");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to display these command options\n\n");
+
+            return builder.toString();
+
+        } else if (setting.equals("LOGGED_IN")) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  logout");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to log out of your account\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  create <NAME>");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to create a new game\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  list");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to list all available games\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  join <ID> [WHITE|BLACK|<empty]");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to join the game with that ID as the selected team\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  observe <ID>");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to observe the game with that ID\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  quit");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to exit\n");
+
+            builder.append(EscapeSequences.SET_TEXT_COLOR_BLUE);
+            builder.append("  help");
+            builder.append(EscapeSequences.RESET_TEXT_COLOR);
+            builder.append(" - to display these command options\n\n");
+
+            return builder.toString();
+        }
     }
 }
